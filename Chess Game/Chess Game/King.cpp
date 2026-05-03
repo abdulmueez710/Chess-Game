ef39f156b793fbdca0f5f9cdb740de01e3f41f82
Kamran_Abbas <kamran.abbas7007@gmail.com>
2026-05-03 08:51:08 +0500

    Update King.cpp

--- Chess Game/Chess Game/King.cpp
+++ Chess Game/Chess Game/King.cpp
@@ -1,8 +1,8 @@
-//#include "King.h"
-//
-//King::King(bool isWhite) : Piece(isWhite) {}
-//
-//bool King::isValidMove(int x1, int y1, int x2, int y2)
-//{
-//	// Movment Logic here
-//}
+#include "King.h"
+
+King::King(bool isWhite) : Piece(isWhite) {}
+
+bool King::isValidMove(int x1, int y1, int x2, int y2)
+{
+	// Movment Logic here
+}
