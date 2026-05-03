32f25272c9644fe63d4995f6e76f454f19784437
Kamran_Abbas <kamran.abbas7007@gmail.com>
2026-05-03 08:49:17 +0500

    Override isValidMove method in King class

--- Chess Game/Chess Game/King.h
+++ Chess Game/Chess Game/King.h
@@ -5,7 +5,7 @@
 class King : public Piece {
 public:
    King(bool isWhite);
-   bool isValidMove(int x1, int y1, int x2, int y2, Board *);
+   bool isValidMove(int x1, int y1, int x2, int y2, Board *board) override ;
 };
 
 #endif
