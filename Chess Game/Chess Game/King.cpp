41408bde25024182d8293052bae10d48136e846d
Kamran_Abbas <kamran.abbas7007@gmail.com>
2026-05-03 08:54:15 +0500

    Initialize symbol for King piece based on color

--- Chess Game/Chess Game/King.cpp
+++ Chess Game/Chess Game/King.cpp
@@ -1,6 +1,7 @@
 #include "King.h"
+#include "Board.h"
 
-King::King(bool isWhite) : Piece(isWhite) {}
+King::King(bool isWhite) : Piece(isWhite) { symbol = isWhite ? 'Q' : 'q';}
 
 bool King::isValidMove(int x1, int y1, int x2, int y2)
 {
