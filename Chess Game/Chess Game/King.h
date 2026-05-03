9189f240fc01d8f3087c92bbdef41efe48b00197
Kamran_Abbas <kamran.abbas7007@gmail.com>
2026-05-03 08:46:47 +0500

    Changes in King.h

--- Chess Game/Chess Game/King.h
+++ Chess Game/Chess Game/King.h
@@ -1,11 +1,11 @@
-//#ifndef KING_H
-//#define KING_H
-//#include "Piece.h"
-//
-//class King : public Piece {
-//public:
-//    King(bool isWhite);
-//    bool isValidMove(int x1, int y1, int x2, int y2);
-//};
-//
-//#endif
+#ifndef KING_H
+#define KING_H
+#include "Piece.h"
+
+class King : public Piece {
+public:
+   King(bool isWhite);
+   bool isValidMove(int x1, int y1, int x2, int y2);
+};
+
+#endif
