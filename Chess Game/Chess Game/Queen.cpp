089a1e2960d255839f835c2415680811797782d3
Kamran_Abbas <kamran.abbas7007@gmail.com>
2026-05-06 21:33:53 +0500

    Update Queen.cpp

--- Chess Game/Chess Game/Queen.cpp
+++ Chess Game/Chess Game/Queen.cpp
@@ -1,8 +1,10 @@
-// #include "Queen.h"
-//
-// Queen::Queen(bool isWhite) : Piece(isWhite) {}
-//
-// bool Queen::isValidMove(int x1, int y1, int x2, int y2)
-//{
-//	// Movment Logic here
-// }
+#include "Queen.h"
+
+Queen::Queen(bool isWhite) : Piece(isWhite) {
+  isWhite ? 'Q' : 'q';
+}
+
+bool Queen::isValidMove(int x1, int y1, int x2, int y2)
+{
+	// Movment Logic here
+}
